d580d2925ec61aa971f76945bb787d8484d14fe9
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-02 16:34:04 -0500

    changing prices on DOM

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ $(document).ready(function() {
   };
 
   var fruits = [apple, orange, banana, grape];
-
+  updatePrice();
   createFruits();
 
 //function for putting fruits on the dom and in inventory
   function createFruits() {
   for(var i = 0; i < fruits.length; i++) {
     $("#container").append('<div class="fruit" id="' + fruits[i].type +
-    '"><button class="sellButton">Buy</button><p class="price">Price</p></div>'
+    '"><button class="sellButton">Buy</button><p class="price">Price: '+ fruits[i].price + '</p></div>'
   );
     $("#inventory").append('<div class="fruit-basket"></div>');
     var $el = $("#inventory").children().last();
@@ -36,28 +36,26 @@ $(document).ready(function() {
   }
 }
 
-updatePrice();
-
 function chooseNewPrice() {
   for (var i = 0; i < fruits.length; i++) {
     fruits[i].price += randomNumber(-50, 50);
     if(fruits[i].price <= 0) {
       fruits[i].price = 0.54;
+      $('.price').empty().html('Price: '+ fruits[i].price);
       return fruits[i].price
     } else {
+      $('.price').empty().html('Price: '+ fruits[i].price);
       return fruits[i].price
     }
   }
 }
 
 function updatePrice() {
-  setInterval(chooseNewPrice, 1500);
+  setInterval(chooseNewPrice, 150);
 }
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min) / 100;
 }
-
-
 
 })
